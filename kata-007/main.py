3466ed12cd5acd125f2d51b9bd33830e034185a2
Marcos Lopez <marcs.lopz@gmail.com>
2026-09-15 21:32:15 +0200

    kata 7 stage 2 completed

--- kata-007/main.py
+++ kata-007/main.py
@@ -1,50 +1,153 @@
 import datetime
 import threading
+import uuid
 from dataclasses import dataclass, field
+
 
 class ServiceException(Exception):
     pass
+
+
 class BookingConflict(ServiceException):
     pass
+
+
 class RoomBookingsNotFound(ServiceException):
     pass
 
+class RoomBookingNotFound(ServiceException):
+    pass
+
+class RoomBookingCancelConflict(ServiceException):
+    pass
+
+
 @dataclass
+class Booking:
+    start: datetime.datetime
+    end: datetime.datetime
+    booking_id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+    def __eq__(self, other):
+        if isinstance(other, Booking):
+            return self.booking_id == other.booking_id
+        return str(self.booking_id) == other
+
+
 @dataclass
 class RoomBookings:
     room_id: str
-    bookings: list[tuple[datetime.datetime, datetime.datetime]] = field(default_factory=list)
+    user_id: str
+    bookings: list[Booking] = field(
+        default_factory=list
+    )
     lock: threading.Lock = field(default_factory=threading.Lock)
 
-    def add_booking(self, start: datetime.datetime, end: datetime.datetime) -> None:
+    def add_booking(self, booking: Booking) -> None:
         with self.lock:
-            for current_start, current_end in self.bookings:
-                if max(current_start, start) < min(current_end, end):
-                    raise BookingConflict(self.room_id, start, end)
-            self.bookings.append((start, end))
+            for current_booking in self.bookings:
+                if max(current_booking.start, booking.start) < min(current_booking.end, booking.end):
+                    raise BookingConflict(self.room_id, booking.start, booking.end)
+            self.bookings.append(booking)
+
+    def list_bookings(self) -> list[Booking]:
+        with self.lock:
+            bookings_list = list(self.bookings)
+        return bookings_list
+
+    def cancel_booking(self, booking_id):
+        with self.lock:
+            self.bookings.remove(booking_id)
+
+
+@dataclass
+class UserBookings:
+    user_id: str
+    bookings: list[Booking] = field(
+        default_factory=list
+    )
+    lock: threading.Lock = field(default_factory=threading.Lock)
+
+    def add_booking(
+        self, booking: Booking
+    ) -> None:
+        with self.lock:
+            self.bookings.append(booking)
 
     def list_bookings(self):
         with self.lock:
             bookings_list = list(self.bookings)
         return bookings_list
 
+    def cancel_booking(self, booking_id):
+        with self.lock:
+            self.bookings.remove(booking_id)
+
 
 class RoomBookingService:
     def __init__(self) -> None:
         self._room_bookings: dict[str, RoomBookings] = dict()
+        self._user_bookings: dict[str, UserBookings] = dict()
+        self._bookings_by_id: dict[str, tuple[Booking, str, str]] = dict()
         self._lock = threading.Lock()
 
-    def create_booking(self, room_id: str, start: datetime.datetime, end: datetime.datetime) -> None:
+    def create_booking(
+        self,
+        room_id: str,
+        user_id: str,
+        start: datetime.datetime,
+        end: datetime.datetime,
+    ) -> str:
+        booking = Booking(start, end)
         with self._lock:
             if room_id not in self._room_bookings:
-                self._room_bookings[room_id] = RoomBookings(room_id, list([(start, end)]))
-                return
+                self._room_bookings[room_id] = RoomBookings(
+                    room_id, user_id, list([booking])
+                )
+                self._user_bookings[user_id] = UserBookings(
+                    user_id, list([booking])
+                )
+                self._bookings_by_id[str(booking.booking_id)] = (booking, room_id, user_id)
+                return str(booking.booking_id)
             room_bookings = self._room_bookings[room_id]
-        room_bookings.add_booking(start, end)
+            user_bookings = self._user_bookings[user_id]
 
-    def list_bookings(self, room_id: str) -> list[tuple[datetime.datetime, datetime.datetime]]:
+        room_bookings.add_booking(booking)
+        user_bookings.add_booking(booking)
+        return str(booking.booking_id)
+
+    def list_bookings_by_room_id(
+        self, room_id: str
+    ) -> list[Booking]:
         with self._lock:
             if room_id not in self._room_bookings:
                 raise RoomBookingsNotFound(room_id)
             room_bookings = self._room_bookings[room_id]
         return room_bookings.list_bookings()
+
+    def list_bookings_by_user_id(
+        self, user_id: str
+    ) -> list[Booking]:
+        with self._lock:
+            if user_id not in self._user_bookings:
+                raise RoomBookingsNotFound(user_id)
+            room_bookings = self._user_bookings[user_id]
+        return room_bookings.list_bookings()
+
+    def cancel_booking(self, booking_id: str, room_id: str, user_id: str) -> None:
+        with self._lock:
+            if booking_id not in self._bookings_by_id:
+                raise RoomBookingNotFound(booking_id)
+            booking, booking_room_id, booking_user_id = self._bookings_by_id[booking_id]
+            try:
+                room_bookings = self._room_bookings[room_id]
+                user_bookings = self._user_bookings[user_id]
+            except KeyError:
+                raise RoomBookingCancelConflict(room_id, user_id)
+
+        room_bookings.cancel_booking(booking_id)
+        user_bookings.cancel_booking(booking_id)
+
+        with self._lock:
+            del self._bookings_by_id[str(booking.booking_id)]
+
--- kata-007/test.py
+++ kata-007/test.py
@@ -2,48 +2,82 @@ import datetime
 
 import pytest
 
-from main import RoomBookingService, BookingConflict
+from main import RoomBookingService, BookingConflict, RoomBookingCancelConflict
 
 
 def test_create_booking_first_time_ok():
     service = RoomBookingService()
-    start_time = (datetime.datetime.now())
+    start_time = datetime.datetime.now()
     end_time = start_time + datetime.timedelta(hours=1)
 
-    service.create_booking("room_1", start_time, end_time)
+    service.create_booking("room_1", "user_1", start_time, end_time)
 
-    bookings_list = service.list_bookings("room_1")
+    bookings_list = service.list_bookings_by_room_id("room_1")
     assert len(bookings_list) == 1
-    assert bookings_list[0][0] == start_time
-    assert bookings_list[0][1] == end_time
+    assert bookings_list[0].start == start_time
+    assert bookings_list[0].end == end_time
+
 
 def test_create_booking_not_conflicting_booking_ok():
     service = RoomBookingService()
-    start_time_1 = (datetime.datetime.now())
+    start_time_1 = datetime.datetime.now()
     end_time_1 = start_time_1 + datetime.timedelta(hours=1)
-    service.create_booking("room_1", start_time_1, end_time_1)
+    service.create_booking("room_1", "user_1", start_time_1, end_time_1)
     start_time_2 = end_time_1
     end_time_2 = start_time_2 + datetime.timedelta(hours=1)
 
-    service.create_booking("room_1", start_time_2, end_time_2)
+    service.create_booking("room_1", "user_1", start_time_2, end_time_2)
 
-    bookings_list = service.list_bookings("room_1")
+    bookings_list = service.list_bookings_by_room_id("room_1")
     assert len(bookings_list) == 2
-    assert (start_time_1, end_time_1) in bookings_list
-    assert (start_time_2, end_time_2) in bookings_list
+
 
 def test_create_booking_conflicting_booking():
     service = RoomBookingService()
-    start_time_1 = (datetime.datetime.now())
+    start_time_1 = datetime.datetime.now()
     end_time_1 = start_time_1 + datetime.timedelta(hours=1)
-    service.create_booking("room_1", start_time_1, end_time_1)
+    service.create_booking("room_1", "user_1", start_time_1, end_time_1)
     start_time_2 = start_time_1 + datetime.timedelta(minutes=15)
     end_time_2 = start_time_2 + datetime.timedelta(hours=1)
 
     with pytest.raises(BookingConflict):
-        service.create_booking("room_1", start_time_2, end_time_2)
+        service.create_booking("room_1", "user_1", start_time_2, end_time_2)
 
-    bookings_list = service.list_bookings("room_1")
+    bookings_list = service.list_bookings_by_room_id("room_1")
     assert len(bookings_list) == 1
-    assert bookings_list[0][0] == start_time_1
-    assert bookings_list[0][1] == end_time_1
+    assert bookings_list[0].start == start_time_1
+    assert bookings_list[0].end == end_time_1
+
+
+def test_cancel_booking():
+    service = RoomBookingService()
+    booking_id = service.create_booking(
+        "room_1", "user_1", datetime.datetime.now(), datetime.datetime.now()
+    )
+
+    service.cancel_booking(booking_id, "room_1", "user_1")
+
+
+def test_cancel_booking_with_different_user():
+    service = RoomBookingService()
+    booking_id = service.create_booking(
+        "room_1", "user_1", datetime.datetime.now(), datetime.datetime.now()
+    )
+
+    with pytest.raises(RoomBookingCancelConflict):
+        service.cancel_booking(booking_id, "room_1", "user_2")
+
+    booking_list_by_room = service.list_bookings_by_room_id("room_1")
+    booking_list_by_user = service.list_bookings_by_user_id("user_1")
+    assert len(booking_list_by_room) == 1
+    assert len(booking_list_by_user) == 1
+
+
+def test_cancel_booking_with_different_room():
+    service = RoomBookingService()
+    booking_id = service.create_booking(
+        "room_1", "user_1", datetime.datetime.now(), datetime.datetime.now()
+    )
+
+    with pytest.raises(RoomBookingCancelConflict):
+        service.cancel_booking(booking_id, "room_2", "user_1")
